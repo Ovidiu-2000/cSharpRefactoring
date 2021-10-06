d5f0b7464bae21aa9b8a00be6039677ee32b8c10
Ovidiu Domanciuc [Intern] <Ovidiu.Domanciuc@endava.com>
2021-10-06 15:04:11 +0300

    Add test for legendary items

---
+++ GildedRoseTest/GildedRoseTestForLegendaryItemsShould.cs
@@ -0,0 +1,30 @@
+﻿using csharp;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRoseTest
+{
+    class GildedRoseTestForLegendaryItemsShould
+    {
+        [Test]
+        [TestCase("Sulfuras, Hand of Ragnaros", 10, 45, 10, 45)]
+        public void SellInAndQualityRemainsTheSameForLegendaryItems(string Name, int SellIn,
+           int Quality, int ExpectedSellIn, int ExpectedQuality)
+        {
+            IList<Item> items = new List<Item>()
+            {
+                new Item(){ Name = Name, SellIn = SellIn, Quality = Quality}
+            };
+
+            GildedRose gildedRose = new GildedRose(items);
+            gildedRose.UpdateQuality();
+
+            Assert.That(items[0].SellIn, Is.EqualTo(ExpectedSellIn));
+            Assert.That(items[0].Quality, Is.EqualTo(ExpectedQuality));
+        }
+    }
+}
